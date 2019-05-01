97803341ffc86eb4c6427f45307438139e4188e8
Ankitkumar Singh <17850047+Anki7singh@users.noreply.github.com>
2019-05-01 22:05:51 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,6 @@
+from keras.models import Sequential
+ from keras.layers import Dense, Activation
+model = Sequential([
+ Dense(32, input_shape=(10,), activation='relu'),
+ Dense(2, activation='softmax'),
+ ])
